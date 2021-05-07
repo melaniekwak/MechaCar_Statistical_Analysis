21629aba26c5d5dbfd171e16d43c2f2bfca2563f
melaniekwak <melaniekwak13@gmail.com>
2021-05-07 14:33:06 -0700

    deliverable1

---
+++ mechacar/deliverable 1.R
@@ -0,0 +1,15 @@
+#Use the library() function to load the dplyr package.
+library(dplyr)
+
+#Import and read in the MechaCar_mpg.csv file as a dataframe.
+read.csv("../MechaCar_mpg.csv")
+
+#Perform linear regression using the lm() function. In the lm() function, pass in all six variables (i.e., columns), 
+#and add the dataframe you created in Step 4 as the data parameter.
+
+
+#Using the summary() function, determine the p-value and the r-squared value for the linear regression model.
+
+
+#Save your MechaCarChallenge.RScript file to your GitHub repository.
+
